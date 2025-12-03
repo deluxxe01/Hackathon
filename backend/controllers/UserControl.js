b280a9f118d1d95f29697353db3e159f6ae17804
CafeinaC4 <vitordanielli3113@gmail.com>
2025-12-03 17:14:21 -0300

    Primeira atualização da branch para jwt

--- backend/controllers/UserControl.js
+++ backend/controllers/UserControl.js
@@ -1,153 +1,193 @@
 import UserModel from "../models/UserModel.js";
 import bcrypt from "bcrypt";
-import genToken from "../util/jwtRoute.js";
 import MedidasModel from "../models/MedidasModel.js";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export default {
+  async CreateUser(req, res) {
+    const { nome, email, senha, data_nascimento, genero } = req.body;
 
-    async CreateUser(req, res) {
+    const salt = bcrypt.genSaltSync(10);
 
-        const { nome, email, senha, data_nascimento, genero } = req.body;
+    const hashedPassword = bcrypt.hashSync(senha, salt);
 
-        const salt = bcrypt.genSaltSync(10);
+    const consulta = await UserModel.createUser({
+      nome,
+      email,
+      hashedPassword,
+      data_nascimento,
+      genero,
+    });
 
-        const hashedPassword = bcrypt.hashSync(senha, salt);
+    const payload = { id: consulta.id, email: consulta.email };
 
-        const consulta = await UserModel.createUser({ nome, email, hashedPassword, data_nascimento, genero });
+    return res.status(201).json({ user: consulta, token });
+  },
 
-        
-        console.log('LOG DO TOKEN',token)
+  async loginUser(req, res) {
+    const { email, senha } = req.body;
 
-        return res.status(201).json({ user: consulta, token });
-    },
+    const user = await UserModel.getUserByEmail(email);
 
-    async loginUser(req, res) {
-        const { email, senha } = req.body;
+    if (!user) {
+      return res.status(404).json({ message: "Usuario não encontrado" });
 
-        const user = await UserModel.getUserByEmail(email);
-
-        if (!user) {
-            return res.status(404).json({ message: "Usuario não encontrado" });
-
-            
-        }
-       
-        const isPasswordValid = bcrypt.compareSync(senha, user.senha);
-        
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Senha incorreta" });
-        }
-
-
-        return res.status(200).json({ user, token });
-    },
-
-    async updateProfile(req, res) {
-        
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            return res.status(401).json({ message: "Sem token de autorização" });
-        }
-
-        const token = authHeader.split(" ")[1];
-
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            const updateFields = req.body;
-
-            const updatedUser = await UserModel.updateProfile(decoded.email, updateFields);
-
-            return res.status(200).json(updatedUser);
-
-        } catch (err) {
-            return res.status(401).json({ message: "Token invalido" });
-        }
-    },
-
-    async getProfile(req, res) {
-        
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            return res.status(401).json({ message: "Sem token de autorizacao" });
-        }
-
-        const token = authHeader.split(" ")[1];
-
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            const user = await UserModel.getUserByEmail(decoded.email);
-
-            if (!user) {
-                return res.status(404).json({ message: "Usuario não encontrado" });
-            }
-
-            return res.status(200).json({ id: user.id, nome: user.nome, email: user.email, data_nascimento: user.data_nascimento, genero: user.genero, nivel_atual: user.nivel_atual, xp_total: user.xp_total, xp_mensal: user.xp_mensal });
-
-        } catch (err) {
-            return res.status(401).json({ message: "Token invalido" });
-        }
-    },
-     async createInfo(req,res){
-
-        const {peso,altura,largura_abdomen,id_usuario} = req.body
-
-        const consulta = await MedidasModel.CreateInfos({id_usuario,peso,altura,largura_abdomen})
-
-        return res.status(201).json(consulta)
-
-    
-     },
-     async updateMedidas(req,res){
-
-        const {peso,altura,largura_abdomen}= req.body
-
-        const id_usuario = req.params.id
-
-        const consulta = await MedidasModel.updateMedidas({id_usuario,altura,peso,largura_abdomen})
-
-        return res.status(200).json(consulta)
-
-     },
-     async getMedidas(req,res){
-
-        const id = req.params.id
-
-        const consulta = await MedidasModel.getInfos(id)
-
-        return res.status(200).json(consulta)
-    },
-    async obtainXp(req,res){
-
-        const {xp} = req.body
-
-        const id = req.params.id
-
-        const consulta = await UserModel.getXp({xp,id})
-
-        return res.status(200).json(consulta)
-
-    },
-
-    async verifyToken(req, res) {
-        const authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            return res.status(401).json({ message: "Sem token de autorização"})
-        }
-        
-        const token = authHeader.split(" ")[1];
-
-        try {
-            const decode = jwt.verify(token, process.env.JWT_SECRET)
-            return res.status(200).json({ valid:true, decode })
-        } catch(err) {
-            return res.status(401).json({ message: "token invalido"})
-        }
+      //
     }
-}
+
+    const isPasswordValid = bcrypt.compareSync(senha, user.senha);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Senha incorreta" });
+    }
+    const token = this.createToken({ id: user.id, email: user.email });
+
+    return res.status(200).json({ user, token });
+  },
+
+  async updateProfile(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "Sem token de autorização" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, secretKey);
+
+      const updateFields = req.body;
+
+      const updatedUser = await UserModel.updateProfile(
+        decoded.email,
+        updateFields
+      );
+
+      return res.status(200).json(updatedUser);
+    } catch (err) {
+      return res.status(401).json({ message: "Token invalido" });
+    }
+  },
+  async deleteUser(req, res) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: "Sem token de autorização" });
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        const deletedUser = await UserModel.deleteUser(decoded.email);
+        if (!deletedUser) {
+            return res.status(404).json({ message: "Usuário não encontrado" });
+        }
+        return res.status(200).json(deletedUser);
+    }
+    catch (err) {
+        return res.status(401).json({ message: "Token inválido" });
+    }
+},
+  async getProfile(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "Sem token de autorizacao" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, secretKey);
+
+      const user = await UserModel.getUserByEmail(decoded.email);
+
+      if (!user) {
+        return res.status(404).json({ message: "Usuario não encontrado" });
+      }
+
+      return res.status(200).json({
+        id: user.id,
+        nome: user.nome,
+        email: user.email,
+        data_nascimento: user.data_nascimento,
+        genero: user.genero,
+        nivel_atual: user.nivel_atual,
+        xp_total: user.xp_total,
+        xp_mensal: user.xp_mensal,
+      });
+    } catch (err) {
+      return res.status(401).json({ message: "Token invalido" });
+    }
+  },
+  async createInfo(req, res) {
+    const { peso, altura, largura_abdomen, id_usuario } = req.body;
+
+    const consulta = await MedidasModel.CreateInfos({
+      id_usuario,
+      peso,
+      altura,
+      largura_abdomen,
+    });
+
+    return res.status(201).json(consulta);
+  },
+  async updateMedidas(req, res) {
+    const { peso, altura, largura_abdomen } = req.body;
+
+    const id_usuario = req.params.id;
+
+    const consulta = await MedidasModel.updateMedidas({
+      id_usuario,
+      altura,
+      peso,
+      largura_abdomen,
+    });
+
+    return res.status(200).json(consulta);
+  },
+  async getMedidas(req, res) {
+    const id = req.params.id;
+
+    const consulta = await MedidasModel.getInfos(id);
+
+    return res.status(200).json(consulta);
+  },
+  async obtainXp(req, res) {
+    const { xp } = req.body;
+
+    const id = req.params.id;
+
+    const consulta = await UserModel.getXp({ xp, id });
+
+    return res.status(200).json(consulta);
+  },
+
+  async createToken(req, res) {
+    const { payload } = req.body;
+    try {
+        const secretKey = crypto.randomBytes(64).toString("hex");
+        console.log("LOG DO TOKEN", token);
+        const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+        return res.json({ token, secretKey });
+    } catch (error) {
+        console.error("Erro ao criar o token:", error);
+    }
+  },
+
+  async TokenVerify(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "Sem token de autorização" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      return res.status(200).json({ valid: true, decoded });
+    } catch (error) {
+      return res.status(401).json({ valid: false, message: "Token inválido" });
+    }
+  },
+};
--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,10 @@
 import express from "express"
 import rotasUser from "../routes/userRoute.js"
-import cors from "cors"
+import admRoutes from "../routes/admRoute.js"
+import missionsRoutes from "../routes/missionsRoute.js"
+import genJwt from "../util/jwtRoute.js"
+//import historicoMissaoRoutes from "../routes/historicoMissaoRoute.js"
+
 const App = express()
 
 App.use(express.json())
@@ -8,7 +12,7 @@ App.use(cors({
     origin: "http://localhost:5173"
   }));
 
-App.use(rotasUser)
+App.use(rotasUser, admRoutes, missionsRoutes, genJwt)
 
 App.listen(3000,()=>{
 
